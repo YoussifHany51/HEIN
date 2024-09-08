63cfbc26cbf31eec32e7b67da3bb18e048a7e86e
Marco Zakaria <marcozakaria@yahoo.com>
2024-09-08 22:17:42 +0300

    Added Address Logic

--- HEIN/Modules/ MasterScene/MeScreen/subModules/Addresses/view/AddressesTableViewCell.swift
+++ HEIN/Modules/ MasterScene/MeScreen/subModules/Addresses/view/AddressesTableViewCell.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class AddressesTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var addressPhone: UILabel!
+    @IBOutlet weak var addressName: UILabel!
+    @IBOutlet weak var isDefault: UISwitch!
+    @IBOutlet weak var addressCountry: UILabel!
+    @IBOutlet weak var addressCity: UILabel!
+    @IBOutlet weak var addressStreet: UILabel!
+    
+    var changeDefaultAddress : (() -> Void) = {}
+    var editAddress : (() -> Void) = {}
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
 
+    @IBAction func changeDefaultAddress(_ sender: Any) {
+        changeDefaultAddress()
+    }
+
+    @IBAction func editAddressButtonAction(_ sender: Any) {
+        editAddress()
+    }
 }
