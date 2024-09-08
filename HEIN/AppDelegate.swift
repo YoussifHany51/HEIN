daeae9ee8d6ea40f6700cd7d0c775be307cebf3b
Youssif Hany <youssifhany962@gmail.com>
2024-09-08 18:31:15 +0300

    Fix Login/Sign Up screen

--- HEIN/AppDelegate.swift
+++ HEIN/AppDelegate.swift
@@ -16,10 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate{
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let providerFactory = AppCheckDebugProviderFactory()
+        AppCheck.setAppCheckProviderFactory(providerFactory)
         FirebaseApp.configure()
         return true
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
+    }
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- HEIN/Modules/Login/View/LoginViewController.swift
+++ HEIN/Modules/Login/View/LoginViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import FirebaseAuth
 import Firebase
+import GoogleSignIn
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButtonOutlet: UIButton!
@@ -22,34 +23,74 @@ class LoginViewController: UIViewController {
         passwordTextField.textContentType = .password
         emailTextField.textContentType = .emailAddress
     }
-
+    
     @IBAction func DoNotHaveAccountButton(_ sender: Any) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "SignUpViewController")
         self.navigationController?.pushViewController(vc!, animated: true)
     }
     @IBAction func loginButton(_ sender: Any) {
-        if viewModel.isInvalidTextFields(email: emailTextField.text!, password: passwordTextField.text!){
-            //Reachability
+        if !viewModel.isInvalidTextFields(email: emailTextField.text!, password: passwordTextField.text!) {
             
-            FirebaseAuth.Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {[weak self] result, error in
-
-                FirebaseAuth.Auth.auth().createUser(withEmail: self!.emailTextField.text!, password: self!.passwordTextField.text!) {[weak self] result, error in
-                    let master = self?.storyboard?.instantiateViewController(withIdentifier: "master")
-                    self?.navigationController?.pushViewController(master!, animated: true)
-                    print("Logged in Success")
+            Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) {[weak self] authResult, error in
+                guard error == nil else{
+                    let error = error as NSError?
+                    switch AuthErrorCode(rawValue: error!.code) {
+                    case .wrongPassword:
+                        self?.showAlert(message: "Wrong Email or Password❗️")
+                        break
+                    case .invalidUserToken: #warning("Check User NOT found")
+                        self?.showAlert(message: "Please Sign Up First")
+                        break
+                    case .networkError:
+                        self?.showAlert(message: "Network Connection Failed❗️ 😞")
+                        break
+                    default:
+                        self?.showAlert(message: "Error: \(error!.localizedDescription)")
+                        print("Error: \(error!.localizedDescription)")
+                        break
+                    }
+                    return
                 }
+                // Successful sign-in, navigate to the master view controller
+                let master = self?.storyboard?.instantiateViewController(withIdentifier: "master")
+                self?.navigationController?.pushViewController(master!, animated: true)
+                print("Logged in Successfully")
             }
-        }else{
-            showAlert()
-            print("Logged in Failed in TextFields")
+        } else {
+            self.showAlert(message: "Please fill in all fields correctly.")
         }
     }
     
     @IBAction func googleSignInButton(_ sender: Any) {
-        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { result, error in
+          guard error == nil else {
+              return
+          }
+
+          guard let user = result?.user,
+            let idToken = user.idToken?.tokenString
+          else {
+              return
+          }
+
+          let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+
+            Auth.auth().signIn(with: credential) { result, error in
+
+              // At this point, our user is signed in
+            }
+        }
     }
-    func showAlert(){
-        let alert = UIAlertController(title: "Error", message: "Invalid Text Fields", preferredStyle: .alert)
+    func showAlert(message:String){
+        let alert = UIAlertController(title: "Error ⚠️", message: message, preferredStyle: .alert)
         let okayButton = UIAlertAction(title: "Okay", style: .default)
         alert.addAction(okayButton)
         self.present(alert, animated: true)
