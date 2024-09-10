b689e1ce4a64908bfb5abfbd13e5ad5543c161f6
Marco Zakaria <marcozakaria@yahoo.com>
2024-09-10 11:28:35 +0300

    Added Updating Cart Logic

--- HEIN/Modules/ MasterScene/CartScreen/viewModel/CartScreenViewModel.swift
+++ HEIN/Modules/ MasterScene/CartScreen/viewModel/CartScreenViewModel.swift
@@ -16,6 +16,8 @@ class CartScreenViewModel {
     
     var draftOrder : DraftOrder? {
         didSet {
+            let filteredDraft = filterLineItems(lineItems: draftOrder!.lineItems)
+            draftOrder?.lineItems = filteredDraft
             bindResultToViewController()
         }
     }
@@ -25,6 +27,8 @@ class CartScreenViewModel {
         case sub
     }
     
+    let dummyLineItem: [String: Any] = ["title": "dummy", "quantity": 1, "price": "0.0", "properties":[]]
+    
     init() {
         nwService = NetworkManager()
         // MARK: - Change to get from userDefaults
@@ -33,12 +37,35 @@ class CartScreenViewModel {
     
     func getDraftOrder() {
         nwService.fetch(url: APIHandler.urlForGetting(.draftOrder(id: "\(draftOrderId)")), type: DraftOrderContainer.self) { draftOrderContainer in
-            //guard let draftOrderContainer = draftOrderContainer else {return}
+            
             self.draftOrder = draftOrderContainer?.draftOrder
         }
     }
     
-    func updateDraftOrder(lineItems: [LineItem], operation: Operation) {
-//        nwService.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: draftOrderId)), parameters: ["draft_order":["line_items":lineItems]])
+    func updateDraftOrder(lineItems: [LineItem]) {
+        nwService.putInApi(url: APIHandler.urlForGetting(.draftOrder(id: "\(draftOrderId)")), parameters: ["draft_order":["line_items": lineItems.count != 0 ? extractLineItemsPutData(lineItems: lineItems) : [dummyLineItem] ]])
+    }
+    
+    func extractLineItemsPutData(lineItems: [LineItem]) -> [[String: Any]]{
+        var result: [[String: Any]] = []
+        for item in lineItems{
+            var properties : [[String: String]] = []
+            for property in item.properties {
+                properties.append(["name":property.name, "value": property.value])
+            }
+            result.append(["variant_id": item.variantID!, "quantity": item.quantity, "properties": properties])
+        }
+                
+        return result
+    }
+    
+    func filterLineItems(lineItems: [LineItem]) -> [LineItem] {
+        var filteredItems : [LineItem] = []
+        for item in lineItems {
+            if item.title != "dummy" {
+                filteredItems.append(item)
+            }
+        }
+        return filteredItems
     }
 }
